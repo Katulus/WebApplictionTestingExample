3d7ea7a5669bbf8e2d3ac5daedb228c7e152cc23
Jiri Tomek <jiri.tomek@solarwinds.com>
2018-05-07 13:05:29 +0200

    Fixed karma setup and config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -43,9 +43,8 @@ module.exports = function(config) {
 
 
     // test results reporter to use
-    // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'progress'],
+    reporters: ['progress', 'dots', 'kjhtml'],
 
 
     // web server port
--- src/setup_karma.bat
+++ src/setup_karma.bat
@@ -1,3 +1,1 @@
-npm install karma --save-dev
-npm install karma-jasmine --save-dev
-npm install karma-chrome-launcher --save-dev
+npm install
